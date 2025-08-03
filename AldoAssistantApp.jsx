a41bfe7ce834c79177f0f7849c9636ef1b5e3d21
aldoslonder2107-stack <aldoslonder2107@gmail.com>
2025-08-03 20:27:06 +0700

    Create AldoAssistantApp.jsx
    Tambah file AldoAssistantApp.jsx

---
+++ AldoAssistantApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AldoAssistantApp = () => {
+  return (
+    <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
+      <h1>Halo Aldo 👋</h1>
+      <p>Ini adalah asisten pribadi kamu. Yuk ngobrol!</p>
+    </div>
+  );
+};
+
+export default AldoAssistantApp;
