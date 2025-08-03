89606b0a0ce018c433f761d15f386a2bc06eeec2
aldoslonder2107-stack <aldoslonder2107@gmail.com>
2025-08-03 20:25:23 +0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AldoAssistantApp from "./AldoAssistantApp";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AldoAssistantApp />
+  </React.StrictMode>
+);
