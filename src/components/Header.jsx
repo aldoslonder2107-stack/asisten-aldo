fd7d0933ad5048a275351b97c28e0293b67b35ad
aldoslonder2107-stack <aldoslonder2107@gmail.com>
2025-08-04 20:12:45 +0700

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="bg-blue-600 text-white p-4 shadow-lg text-center text-xl font-bold">
+      👋 Selamat datang, Aldo!
+    </div>
+  )
+}
+
+export default Header
