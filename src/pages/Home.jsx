befa35a4fc8d240bea7aea66648fa8dfc4d2410a
aldoslonder2107-stack <aldoslonder2107@gmail.com>
2025-08-04 20:10:57 +0700

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import ChatBox from '../components/ChatBox'
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <ChatBox />
+    </div>
+  )
+}
+
+export default Home
