3e9cefe7910ea21096b5336545479b537fea1dca
aldoslonder2107-stack <aldoslonder2107@gmail.com>
2025-08-04 15:27:55 +0700

    Create app.jsx
    create app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatAssistant from './components/ChatAssistant'; // pastikan path ini sesuai
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <ChatAssistant />
+    </div>
+  );
+}
+
+export default App;
